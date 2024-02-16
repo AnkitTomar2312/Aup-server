855dd28a61134867b5b33155a4c0e73e15d00c06
Ankit Tomar <ankitjdec@gmail.com>
2024-02-16 18:39:44 +0530

    initial set up for backend connect

--- db/config.js
+++ db/config.js
@@ -1,2 +1,7 @@
+// const mongoose = require("mongoose");
+// console.log(process.env.CONNECTION);
+//mongoose.connect("mongodb://127.0.0.1:27017/Aupdeshik");
+//mongoose.connect(process.env.CONNECTION);
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/Aupdeshik");
+require("dotenv").config();
+mongoose.connect(process.env.CONNECTION);
